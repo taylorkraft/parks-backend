6c1e8fa8fbf96b306f255bd9daba753de9c60bd3
taylorkraft <taylorkraft16@gmail.com>
2020-08-08 13:17:34 -0600

    serializer

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,18 +2,21 @@ class StatesController < ApplicationController
 
   def index
     states = State.all
-    render json: StateSerializer.new(states).to_serialized_json
+    # render json: StateSerializer.new(states).to_serialized_json
+    render json: states
   end
 
   def show
     state = State.find(params[:id])
-    render json: StateSerializer.new(state).to_serialized_json
+    # render json: StateSerializer.new(state).to_serialized_json
+    render json: state
   end
 
   def update
     state = State.find(params[:id])
     state.update(state_params(:name, :flower))
-    render json: StateSerializer.new(state).to_serialized_json
+    # render json: StateSerializer.new(state).to_serialized_json
+    render json: state
   end
 
   private
