dd940bcf812710e333e4fbd14dc06e34cc68cee0
taylorkraft <taylorkraft16@gmail.com>
2020-08-15 13:57:26 -0400

    remove notes

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -2,13 +2,11 @@ class StatesController < ApplicationController
 
   def index
     states = State.all
-    # render json: StateSerializer.new(states).to_serialized_json
     render json: states, except: [:created_at, :updated_at]
   end
 
   def show
     state = State.find(params[:id])
-    # render json: StateSerializer.new(state).to_serialized_json
     render json: state
   end
 
@@ -24,7 +22,6 @@ class StatesController < ApplicationController
   def update
     state = State.find(params[:id])
     state.update(state_params(:name, :flower))
-    # render json: StateSerializer.new(state).to_serialized_json
     render json: state
   end
 
