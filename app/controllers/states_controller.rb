1a7542ab33acc5c280d16b3cee02a9ebd0b33b23
taylorkraft <taylorkraft16@gmail.com>
2020-08-16 16:50:14 -0400

    destroy method

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -9,7 +9,6 @@ class StatesController < ApplicationController
     state = State.find(params[:id])
     render json: state, include: [:parks], except: [:created_at, :updated_at]
   end
-  end
 
   def create 
     state = State.new(state_params(:name))
@@ -24,6 +23,11 @@ class StatesController < ApplicationController
     state = State.find(params[:id])
     state.update(state_params(:name))
     render json: state, include: [:parks]
+  end
+
+  def destroy
+    state.delete
+    render json: {state_id: state.id}
   end
 
   private
